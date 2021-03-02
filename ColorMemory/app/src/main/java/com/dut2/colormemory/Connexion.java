876bf72d42d714401590719c06c6c9775d7e59d8
Romain Caron <caron.romain@free.fr>
2021-03-02 16:13:47 +0100

    Game menu
    Ajout du GameMenu

--- ColorMemory/app/src/main/java/com/dut2/colormemory/Connexion.java
+++ ColorMemory/app/src/main/java/com/dut2/colormemory/Connexion.java
@@ -4,15 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.EmptyStackException;
 
 public class Connexion extends AppCompatActivity {
 
+    private EditText mEditTextMail;
+    private EditText mEditTextPassword;
+    private Button mValidButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        mEditTextMail = findViewById(R.id.etm_mail);
+        mEditTextMail = findViewById(R.id.etp_password);
+        mValidButton = findViewById(R.id.btn_valid);
+
+        mValidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(VConnexion()) {
+                    Intent intent = new Intent(Connexion.this, GameMenu.class);
+                    Bundle bundle = new Bundle();
+                    String[] user = new String[4];
+                    String name = ""; String firstName= ""; String mail= ""; String password= "";
+                    user[0] = name; user[1] = firstName; user[2] = mail; user[3] = password;
+                    bundle.putStringArray("user", user);
+                    startActivityForResult(intent, 3000);
+                }
+            }
+        });
+    }
+
+    private boolean VConnexion() {
+        boolean isValid = true;
+
+        //Vérification de l'existance de l'utilisateur dans la base de données
+
+        return isValid;
     }
 }
--- ColorMemory/app/src/main/java/com/dut2/colormemory/GameMenu.java
+++ ColorMemory/app/src/main/java/com/dut2/colormemory/GameMenu.java
@@ -2,13 +2,54 @@ package com.dut2.colormemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class GameMenu extends AppCompatActivity {
+
+    private ImageView mImageViewEasy;
+    private ImageView mImageViewDifficult;
+    private ImageView mImageViewExpert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
+
+        mImageViewEasy = findViewById(R.id.iv_btn_easy);
+        mImageViewDifficult = findViewById(R.id.iv_btn_difficult);
+        mImageViewExpert = findViewById(R.id.iv_btn_expert);
+
+        Intent intent = new Intent(GameMenu.this, Game.class);
+        Bundle bundle = new Bundle();
+        mImageViewEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("mode", "easy");
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 3000);
+            }
+        });
+
+        mImageViewDifficult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("mode", "difficult");
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 3000);
+            }
+        });
+
+        mImageViewExpert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("mode", "expert");
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 3000);
+            }
+        });
+
     }
 }
